f74b05f81ee5e4a9ab0f2c6e6fa5d4abe0caeb39
Jonathan Welzel <jnwelzel@gmail.com>
2024-06-05 22:20:25 +0200

    Updated home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,36 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 export const HomePage: FC = () => {
   return (
-    <div className="grid grid-cols-12">
-      <p className="justify-self-start">Home Page</p>
+    <div className="flex flex-col h-full justify-center items-center">
+      <span>
+        <div className="text-9xl font-extrabold">
+          <h1 className="text-left bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            Olá
+          </h1>
+        </div>
+        <p className="text-left">(that's "hello" in Portuguese)</p>
+      </span>
+      <span className="relative my-10">
+        <span
+          className="block absolute -inset-1 bg-slate-600 w-1"
+          aria-hidden="true"
+        ></span>
+        <p className="ml-2">
+          I'm Jonathan, a Brazilian full stack Javascript developer currently
+          living in Germany with my beautiful wife and two wonderful kids. To
+          learn more about me click{" "}
+          <Link to="/about" className="underline">
+            here
+          </Link>
+          .
+        </p>
+      </span>
+      <p>
+        Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsum lorem ipsum.
+      </p>
     </div>
   );
 };
