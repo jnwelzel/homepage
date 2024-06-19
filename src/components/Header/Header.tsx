13e73c4bf0e58ac51b3cfcb9afde0e6648887934
Jonathan Welzel <jnwelzel@gmail.com>
2024-06-19 21:53:34 +0200

    Modified header, darker

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,7 +17,7 @@ export const Header: FC = () => {
 
   return (
     <>
-      <nav className="shadow bg-white border p-3 z-20 md:w-[584px] md:mx-auto md:inset-x-0 md:mt-4 md:rounded-full md:px-6 md:py-3 fixed w-full">
+      <nav className="bg-gray-700 border-0 shadow-lg md:shadow-none md:border md:border-white p-3 z-20 md:w-[584px] md:mx-auto md:inset-x-0 md:mt-4 md:rounded-full md:px-6 md:py-3 fixed w-full">
         <ul className="flex items-center flex-1">
           <li
             className="bg-yellow-300 rounded size-9 relative overflow-hidden"
@@ -31,13 +31,13 @@ export const Header: FC = () => {
           <li className="ml-auto flex items-center md:hidden">
             <button onClick={toggleSidebar} aria-label="Toggle sidebar button">
               {!isSidebarVisible ? (
-                <Bars2Icon className="size-6" />
+                <Bars2Icon className="size-6 text-white" />
               ) : (
-                <XMarkIcon className="size-6" />
+                <XMarkIcon className="size-6 text-white" />
               )}
             </button>
           </li>
-          <li className="max-md:hidden md:visible ml-auto">
+          <li className="max-md:hidden md:visible ml-auto text-white">
             <NavLink
               to="/blog"
               className={({ isActive }) =>
@@ -46,7 +46,7 @@ export const Header: FC = () => {
               Blog
             </NavLink>
           </li>
-          <li className="max-md:hidden md:visible md:ml-5">
+          <li className="max-md:hidden md:visible md:ml-5 text-white">
             <NavLink
               to="/portfolio"
               className={({ isActive }) =>
@@ -55,7 +55,7 @@ export const Header: FC = () => {
               Portfolio
             </NavLink>
           </li>
-          <li className="max-md:hidden md:visible md:ml-5">
+          <li className="max-md:hidden md:visible md:ml-5 text-white">
             <NavLink
               to="/about"
               className={({ isActive }) =>
@@ -70,12 +70,12 @@ export const Header: FC = () => {
         <div onClick={toggleSidebar} className="z-[9] fixed top-0 right-0 bottom-0 left-0" />
       ) : null}
       <aside
-        className={`bg-white shadow fixed right-0 top-[60px] bottom-0 z-10 slide-in ${isSidebarVisible ? 'slide-out' : ''} md:hidden`}>
+        className={`bg-gray-700 shadow fixed right-0 top-[60px] bottom-0 z-10 slide-in ${isSidebarVisible ? 'slide-out' : ''} md:hidden`}>
         <ul
           className="grid"
           onFocus={() => setIsSidebarVisible(true)}
           onBlur={() => setIsSidebarVisible(false)}>
-          <li className="px-8 py-2">
+          <li className="px-8 py-2 text-white">
             <NavLink
               to="/blog"
               onClick={closeSidebar}
@@ -85,7 +85,7 @@ export const Header: FC = () => {
               Blog
             </NavLink>
           </li>
-          <li className="px-8 py-2">
+          <li className="px-8 py-2 text-white">
             <NavLink
               to="/portfolio"
               onClick={closeSidebar}
@@ -95,7 +95,7 @@ export const Header: FC = () => {
               Portfolio
             </NavLink>
           </li>
-          <li className="px-8 py-2">
+          <li className="px-8 py-2 text-white">
             <NavLink
               to="/about"
               onClick={closeSidebar}
