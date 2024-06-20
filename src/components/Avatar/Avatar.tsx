2e0ae43e113681db82f10d450a44b6df374d45ed
Jonathan Welzel <jnwelzel@gmail.com>
2024-06-20 18:48:37 +0200

    Flipped avatar

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -3,5 +3,10 @@ import { FC } from 'react'
 import portrait from '../../assets/avatar.webp'
 
 export const Avatar: FC = () => {
-  return <img className="rounded-full shadow-lg size-28 inline-flex" src={portrait} />
+  return (
+    <img
+      className="rounded-full shadow-lg size-28 inline-flex scale-x-[-1] -rotate-12"
+      src={portrait}
+    />
+  )
 }
