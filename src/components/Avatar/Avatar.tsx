c69c671c6eda35b3be42441812e303c5a088ed99
Jonathan Welzel <jnwelzel@gmail.com>
2024-06-21 09:40:55 +0200

    Aligned avatar

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -5,7 +5,7 @@ import portrait from '../../assets/avatar.webp'
 export const Avatar: FC = () => {
   return (
     <img
-      className="rounded-full shadow-lg size-28 inline-flex scale-x-[-1] -rotate-12"
+      className="rounded-full shadow-lg size-28 inline-flex scale-x-[-1] -rotate-12 -ml-5"
       src={portrait}
     />
   )
