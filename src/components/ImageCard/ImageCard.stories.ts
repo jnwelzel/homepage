271021d60aa587b85cd26f2f3f18ba6e23148274
Jonathan Welzel <jnwelzel@gmail.com>
2025-09-24 17:34:32 +0200

    Updated image card story

--- src/components/ImageCard/ImageCard.stories.ts
+++ src/components/ImageCard/ImageCard.stories.ts
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
 
+import distanceFromX from '../../assets/distance-from-x.webp'
 import { ImageCard } from './ImageCard'
 
 const meta = {
@@ -13,6 +14,10 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    id: 'carousel-1',
+    imageURL: distanceFromX,
+    title: 'Distance from X',
+    description:
+      "React SPA to calculate the distance between two geographic points. The app also uses Google's Geocoding API for searching and fetching geocoordinates.",
+    linkURL: 'https://jnwelzel.github.io/distance-from-x/',
   },
 }
