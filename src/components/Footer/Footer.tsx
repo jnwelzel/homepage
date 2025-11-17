276cf0f2fb58ce2726862484e35b42a1802f97cb
Jonathan Welzel <jnwelzel@gmail.com>
2025-11-17 14:08:19 +0100

    Add Instagram and WhatsApp links to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { EnvelopeIcon } from '@heroicons/react/24/outline'
 import { FC } from 'react'
+import { FaInstagram, FaWhatsapp } from 'react-icons/fa'
 import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io'
 
 export const Footer: FC = () => {
@@ -8,9 +9,9 @@ export const Footer: FC = () => {
       <div className="bg-gray-700">
         <div className="h-5 bg-white rounded-b-2xl" />
       </div>
-      <footer className="bg-gray-700 text-white foo relative z-10">
-        <ul className="grid gap-4 p-4 grid-cols-3 max-w-[1312px] mx-auto">
-          <li className="md:col-span-3">
+      <footer className="bg-gray-700 text-white relative z-10">
+        <ul className="grid gap-4 px-4 py-12 grid-cols-3 md:flex max-w-[1312px] mx-auto">
+          <li>
             <a
               href="https://github.com/jnwelzel/"
               target="_blank"
@@ -18,7 +19,7 @@ export const Footer: FC = () => {
               <IoLogoGithub className="size-6" /> Github
             </a>
           </li>
-          <li className="justify-self-center md:col-span-3 md:justify-self-start">
+          <li>
             <a
               href="https://www.linkedin.com/in/jonwelzel/"
               target="_blank"
@@ -26,10 +27,24 @@ export const Footer: FC = () => {
               <IoLogoLinkedin className="size-6" /> LinkedIn
             </a>
           </li>
-          <li className="justify-self-end md:col-span-3 md:justify-self-start">
+          <li>
             <a href="mailto:contact@jonwelzel.online" className="flex items-center gap-2">
               <EnvelopeIcon className="size-6" />
               Contact
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/jnwelzel?igsh=ZXNza29sdXBneGVm"
+              className="flex items-center gap-2">
+              <FaInstagram className="size-6" />
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a href="https://wa.me/4917669494588" className="flex items-center gap-2">
+              <FaWhatsapp className="size-6" />
+              WhatsApp
             </a>
           </li>
         </ul>
